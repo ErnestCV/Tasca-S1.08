48f20e3ccfe14fd8f47c0113d3c511283f809f24
ErnestCV <ecv.spook@gmail.com>
2022-06-29 09:47:56 +0200

    Versió revisada

--- n1ex1/src/main/java/org/n1ex1/Main.java
+++ n1ex1/src/main/java/org/n1ex1/Main.java
@@ -1,9 +1,33 @@
 package org.n1ex1;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
 
-        
+        List<String> llista = new ArrayList<>();
+        llista.add("Hola");
+        llista.add("Test");
+        llista.add("Ahoy!");
+        llista.add("Paraula");
+        llista.add("Frase llarga que conté una o");
+        llista.add("Onada");
+
+        retornaStringsAmbO(llista);
+        retornaStringsAmbOIgnoraMajuscules(llista);
+    }
+
+    public static List<String> retornaStringsAmbO(List<String> llista) {
+
+        return llista.stream().filter(s -> s.contains("o")).collect(Collectors.toList());
+
+    }
+
+    public static List<String> retornaStringsAmbOIgnoraMajuscules(List<String> llista) {
+
+        return llista.stream().map(String::toLowerCase).filter(s -> s.contains("o")).collect(Collectors.toList());
 
     }
 }
